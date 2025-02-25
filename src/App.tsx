a2be2d70525a2894dc7a04e2a0544024935c9399
4twentydev <4twenty.dev@gmail.com>
2025-02-24 20:27:16 -0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ function App() {
   return (
     <div className="flex justify-center mt-5">
       <div className="text-center">
-        <h1 className="text-3xl text-green-500 text-center mb-5 font-semibold">
-          ShadeCN Initalized with react 19 and tailwindcss 4
-        </h1>
+        <h1 className="text-3xl text-green-500 text-center mb-5 font-semibold"></h1>
         <Button>Test Button</Button>
       </div>
     </div>
